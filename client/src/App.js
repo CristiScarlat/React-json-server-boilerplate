81c528cbb80212afc9020d2180adadd57baf8678
CristiScarlat <78.cristi@gmail.com>
2021-11-17 18:39:20 +0200

    aad keypress eventListener in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
+import React from 'react'
 import './App.scss';
 
 function App() {
+
+  React.useEffect(() => {
+    document.addEventListener('keypress', keypressHandler)
+    return () => document.removeEventListener('keypress', keypressHandler)
+  }, [])
+
+  const keypressHandler = (key) => {
+    console.log(key)
+  }
+
   return (
     <div className="App">
       app
