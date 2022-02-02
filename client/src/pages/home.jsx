50b1bcf29174b9012f17eda302eaff5d3012cbc1
CristiScarlat <78.cristi@gmail.com>
2022-02-02 18:47:32 +0200

    create a local json server and fetch data from it

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import Carusel from '../components/caruselComponent/caruselComponent';
+import { getQuizData } from '../services/api';
 
 const Home = () => {
+    const [quizData, setQuizData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const res = await getQuizData();
+            if(res.status === 200)setQuizData(res.data);
+            else {
+                throw new Error('Something went wrong. please try again later.')
+            }
+        }
+        try{
+            fetchData()
+        }
+        catch(error){
+            console.error(error);
+        }
+
+    }, [])
+
+    console.log(quizData[0])
+
     return (
     <div>
         <Carusel />
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,27 +1,33 @@
 import axios from 'axios'
 
 const instance = axios.create ({
-    baseURL : 'https://api.coingecko.com/api/v3',
+    //baseURL : 'https://api.coingecko.com/api/v3',
+    baseURL: 'http://localhost:3000',
     headers: {'Accept': 'application/json'}
   })
 
-  function buildParams(obj) {
-    const arr = []
-    Object.keys(obj).forEach(k => {
-        if(obj[k])arr.push(`${k}=${obj[k]}`)
-    })   
-    return arr.join('&')
+//   function buildParams(obj) {
+//     const arr = []
+//     Object.keys(obj).forEach(k => {
+//         if(obj[k])arr.push(`${k}=${obj[k]}`)
+//     })   
+//     return arr.join('&')
+// }
+
+// //https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false
+// async function getCoinsMarket(paramsObj){
+//     const params = buildParams(paramsObj)
+//     return await instance.get(`/coins/markets?${params}`)
+// }
+
+// //https://api.coingecko.com/api/v3/coins/bitcoin
+// async function getCoinDetails(id){
+//     return await instance.get(`/coins/${id}`)
+// }
+
+async function getQuizData(){
+  return await instance.get('/questions');
 }
 
-//https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false
-async function getCoinsMarket(paramsObj){
-    const params = buildParams(paramsObj)
-    return await instance.get(`/coins/markets?${params}`)
-}
-
-//https://api.coingecko.com/api/v3/coins/bitcoin
-async function getCoinDetails(id){
-    return await instance.get(`/coins/${id}`)
-}
-
-export { getCoinsMarket, getCoinDetails }
+// export { getCoinsMarket, getCoinDetails, getQuizData }
+export { getQuizData };
