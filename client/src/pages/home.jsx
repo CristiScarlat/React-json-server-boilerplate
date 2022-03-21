d735509e475e4b1a010197371c4e57ead8cbed7d
CristiScarlat <78.cristi@gmail.com>
2022-03-21 20:32:10 +0200

    add call endpoint for localhost 5000

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Carusel from '../components/caruselComponent/caruselComponent';
+import { getProducts } from '../services/api';
 
 const Home = () => {
+
+    useEffect(() => {
+        getProducts()
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
     return (
     <div>
         <Carusel />
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,27 +1,19 @@
 import axios from 'axios'
 
 const instance = axios.create ({
-    baseURL : 'https://api.coingecko.com/api/v3',
+    baseURL : 'http://localhost:5000/',
     headers: {'Accept': 'application/json'}
   })
 
-  function buildParams(obj) {
-    const arr = []
-    Object.keys(obj).forEach(k => {
-        if(obj[k])arr.push(`${k}=${obj[k]}`)
-    })   
-    return arr.join('&')
+async function getProducts(){
+    try{
+        const res = await instance.get('/products');
+        return res.data
+    } 
+    catch(error){
+        throw new Error(error)
+    }
 }
 
-//https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false
-async function getCoinsMarket(paramsObj){
-    const params = buildParams(paramsObj)
-    return await instance.get(`/coins/markets?${params}`)
-}
 
-//https://api.coingecko.com/api/v3/coins/bitcoin
-async function getCoinDetails(id){
-    return await instance.get(`/coins/${id}`)
-}
-
-export { getCoinsMarket, getCoinDetails }
+export { getProducts }
